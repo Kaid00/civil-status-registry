391e4e1c58a31cd8fb531b82955a8b3feb1d0c78
Jay <depixellife@gmail.com>
2021-06-09 02:14:33 +0100

    Intial commit

---
+++ models/birthModel.js
@@ -0,0 +1,119 @@
+const mongoose = require('mongoose');
+
+const birthSchema = mongoose.Schema(
+  {
+    surname: {
+      type: String,
+      required: [true, 'Why no Surname'],
+      maxLength: [100, 'A surname must be less than or equal to 100'],
+      trim: true,
+    },
+    givenname: {
+      type: String,
+      required: [true, 'Why no Given name'],
+      maxLength: [100, 'A given name must be less than or equal to 100'],
+      trim: true,
+    },
+    born_at: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    born_on: {
+      type: Date,
+      required: true,
+    },
+    sex: {
+      type: String,
+      required: true,
+      enum: {
+        values: ['male', 'female'],
+        message: 'Sex is either male or female ',
+      },
+    },
+    father_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    father_born_at: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    father_born_on: {
+      type: String,
+      required: true,
+    },
+    father_resident_at: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    father_occupation: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    father_nationality: {
+      type: String,
+      required: true,
+      default: 'Cameroon',
+      trim: true,
+    },
+    father_ref_doc: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    mother_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mother_born_at: {
+      type: String,
+      required: [true, 'Mothers place of birth must be specified'],
+      trim: true,
+    },
+    mother_resident_at: {
+      type: String,
+      required: [true, 'Mothers resident must be specified'],
+      trim: true,
+    },
+    mother_occupation: {
+      type: String,
+      trim: true,
+    },
+    mother_born_on: {
+      type: Date,
+      required: [true, 'Mother must have a date of birth'],
+    },
+    mother_ref_doc: {
+      type: Number,
+      required: [true, 'Mother must have a National Id Number'],
+      unique: true,
+    },
+    mother_nationality: {
+      type: String,
+      required: [true, 'Mother must have a nationality'],
+      default: 'Cameroon',
+      trim: true,
+    },
+    drawn_up_on: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+birthSchema.virtual('fullName').get(function () {
+  return this.givenname + ' ' + this.surname;
+});
+
+module.exports = mongoose.model('birth', birthSchema);
---
+++ models/marriageModel.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+
+const marriageSchema = mongoose.Schema(
+  {
+    groom_given_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    groom_surname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    groom_father_name: {
+      type: String,
+      trim: true,
+    },
+    groom_mother_name: {
+      type: String,
+      trim: true,
+    },
+    bride_father_name: {
+      type: String,
+      trim: true,
+    },
+    bride_mother_name: {
+      type: String,
+      trim: true,
+    },
+    groom_date_birth: {
+      type: Date,
+      required: true,
+    },
+    bride_date_birth: {
+      type: Date,
+      required: true,
+    },
+    groom_place_birth: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    groom_id_num: {
+      type: Number,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    bride_place_birth: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bride_id_num: {
+      type: Number,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    bride_given_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bride_surname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    date_of_marriage: {
+      type: Date,
+      required: true,
+    },
+    place_of_marriage: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+marriageSchema.virtual('groom_age', function () {
+  return Date.now().getFullYear - this.groom_date_birth;
+});
+module.exports = mongoose.model('Marriage', marriageSchema);
