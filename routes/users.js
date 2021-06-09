1012a361e8e21b5a459027e29db01d365040cecc
Jay <depixellife@gmail.com>
2021-06-09 02:15:33 +0100

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
+const router = express.Router();
+
+// Create new user
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.allUser);
 
 module.exports = router;
