27f9fd7cdb2c19578d8c8d464bc50b810d83ad20
Jay <depixellife@gmail.com>
2021-06-15 10:43:05 +0100

    Added protection to routes

--- routes/marriage.js
+++ routes/marriage.js
@@ -1,17 +1,18 @@
 const express = require('express');
 
 const marriageController = require('../controllers/marriageController');
+const authController = require('../controllers/authController');
 const router = express.Router();
 
 router
   .route('/marriage')
-  .get(marriageController.getAllCert)
-  .post(marriageController.createCert);
+  .get(authController.protect, marriageController.getAllCert)
+  .post(authController.protect, marriageController.createCert);
 
 router
   .route('/marriage/:id')
-  .get(marriageController.getCert)
-  .patch(marriageController.updateCert)
-  .delete(marriageController.deleteCert);
+  .get(authController.protect, marriageController.getCert)
+  .patch(authController.protect, marriageController.updateCert)
+  .delete(authController.protect, marriageController.deleteCert);
 
 module.exports = router;
