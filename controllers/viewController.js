949da2af0670638cf18b72ccdaf8a3e00cc25df7
Jay <depixellife@gmail.com>
2021-07-14 11:44:44 +0100

    Refactored View Routes Controllers following MVC

---
+++ controllers/viewController.js
@@ -0,0 +1,43 @@
+
+
+exports.dashboard =  (req, res) => {
+    res.status(200).render('dashboard', {
+      title: 'Dashboard'
+    })
+}
+
+exports.birth =  (req, res) => {
+    res.status(200).render('birth', {
+      title: 'Birth'
+    }) 
+  }
+
+exports.marriage = (req, res) => {
+    res.status(200).render('marriage', {
+      title: 'Marriage'
+    })
+  }
+
+exports.upload = (req, res) => {
+    res.status(200).render('upload', {
+      title: 'CSV'
+    })
+  }
+
+exports.home = (req, res) => {
+    res.status(200).render('home', {
+        title: 'CRS',
+        user: 'Azamah Jr'
+    });
+
+}
+
+exports.login = (req, res) => {
+    res.status(200).render('login', {
+        title: 'Login',
+        user: 'Azamah Jr'
+    });
+
+}
+
+
