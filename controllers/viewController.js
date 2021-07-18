e9f2e4fe1113945d55eb86f7ac42ff1ac4f75012
Jay <depixellife@gmail.com>
2021-07-18 03:13:08 +0100

    puppeteer :generates PDF

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,16 +1,17 @@
 const births = require('../models/birthModel')
 const marriages = require('../models/marriageModel')
+const puppeteer = require('puppeteer');
 
 exports.dashboard =  async(req, res) => {
   try {
     const birth  = await births.find()
     const marriage  = await marriages.find();
     const last = await birth[birth.length-1];
-    console.log(last);
+   
 
     const total = birth.length + marriage.length;
-    const birthPercentage = (birth.length / total) * 100;
-    const marriagePercentage = (marriage.length / total) * 100;
+    const birthPercentage = ((birth.length / total) * 100).toFixed(1);
+    const marriagePercentage = ((marriage.length / total) * 100).toFixed(1);
 
 
     res.status(200).render('dashboard', {
@@ -62,7 +63,7 @@ exports.login = (req, res) => {
 
 }
 
-exports.generateBirth = async (req, res) => {
+exports.generateBirth = async (req, res, next) => {
   try {
     const birthData  = await births.find()
     const birth = await birthData[birthData.length-1];
@@ -72,10 +73,66 @@ exports.generateBirth = async (req, res) => {
     title: 'Generated Birth Certificate',
     birth
   })
-  } catch {
 
+  next()
+  } catch(err) {
+    res.status(400).json({
+      status: 'Failed'
+    })
+
+    next()
   }
 
+}
+
+exports.generateBirthPrint = async (req, res, next) => {
+  try {
+    const birthData  = await births.find()
+    const birth = await birthData[birthData.length-1];
+
+
+    res.status(200).render('print_ready', {
+    title: 'Generated Birth Certificate',
+    birth
+  })
+
+  next()
+  } catch(err) {
+    res.status(400).json({
+      status: 'Failed'
+    })
+
+    next()
+  }
+
+}
+
+
+exports.getpuppet = async (req, res, next) => {
+  try {
+    const url = 'http://localhost:3000/print-ready';
+    const browser = await puppeteer.launch()
+    
+    const page = await browser.newPage();
+    await page.goto(url);
+    //  await page.setContent(contenthtml)
+
+    await page.pdf({path: "pdf1.pdf", format: "A3"});
+    await browser.close();
+    res.status(200).json({
+      status: 'success',
+      message: 'PDF saved'
+    })
+
+    return next()
+
+  } catch(err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err
+    })
+    return next()
+  }
 }
 
 
