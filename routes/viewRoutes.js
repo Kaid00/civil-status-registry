900ca1efe9ddc3b0d0c5e783f7e62bc3a2cc1099
Jay <depixellife@gmail.com>
2021-07-18 04:39:43 +0100

    added route to open printer page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const viewController = require('../controllers/viewController')
 const authController = require('../controllers/authController')
 
+
 const router = express.Router();
 
 router.use(authController.isLoggedIn)
@@ -16,7 +17,15 @@ router.get('/marriage',authController.protect, viewController.marriage)
 router.get('/upload',authController.protect, viewController.upload)
 router.get('/login', viewController.login)
 
-router.get('/generate-birth-certificate',authController.protect, viewController.generateBirth )
+router.get('/generate-birth-certificate',authController.protect, viewController.generateBirth, )
+
+router.get('/print-ready', viewController.generateBirthPrint, )
+
+router.get('/createpdf', viewController.getpuppet)
+
+// router.get('/test', viewController.test)
+
+router.get('/pdf', viewController.sendPdf)
 
 
 
